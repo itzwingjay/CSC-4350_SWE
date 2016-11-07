a80ec93d710ea2e6a5a0a3d6bf6fccd607e02602
Lawrence Tsao <ltsao2@student.gsu.edu>
2016-11-06 21:44:48 -0500

    edited

---
+++ core.js
@@ -0,0 +1,30 @@
+var userModule = angular.module('userModule', []);
+
+userModule.controller('mainController', function($scope, $http) {
+    $scope.restData = {};
+
+    //This is where you're calling the get 
+        $http.get('http://localhost:1234/restaurants')
+            .success(function(data) {
+                $scope.restaurants = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    
+
+    //This is where you're calling the post 
+    $scope.addUser = function() {
+        $http.post('http://localhost:1234/restaurants', angular.toJson($scope.userData)) 
+            .success(function(data) {
+                $scope.restData = {}; //clear the user data after the response 
+                $scope.restaurants = $scope.restaurants.concat([data]); //add the user data to our scope variable 
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+});
--- server.js
+++ server.js
@@ -1,41 +1,82 @@
 var mongodb = require('mongodb');
 var express = require('express');
-var app 		= express();
-var bodyParser	= require('body-parser');
-var router		= express.Router();
+var app         =   express();
+var bodyParser  =   require('body-parser');
+var router      =   express.Router();
 
-//End point for the DB when localhosted
-var mongoConnection = "mongodb://localhost:27017/restaurant";
+//Monogo defaults to 27017
+// `mongod --dbpath C:\afolder\mongoStuff` 
+var mongoConnection = "mongodb://localhost:27017/slack";
 
-//global scoped variable for DB
+//Need a  globally scoped variable
 var globalDb;
 
-//open connection and create DB of it doesn't exsit
-mongodb.MongoClient.connect(mongoConnection, function(err,database){
-	if(err){
-		console.log(err);
-	}else{
-		console.log('Awesome, Mongodb is connected!')
-	}
+//This will open the connection and create the DB if it doens't exist
+mongodb.MongoClient.connect(mongoConnection, function (err, database) {
+  if (err) {
+    console.log(err);
+  }else {
+    console.log('Yay! Mongo is doing mongo stuff. That was easy!');
+  }
 
-	globalDb = database;
+  globalDb = database;
+
 });
 
+
+//Body parser is so we can read the payload from a POST without rolling our own 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
-var RESTAURANT_COLLECTION = "restaurant";
-router.get("/restaurants", function(req,res){
-	globalDb.collection(RESTAURANT_COLLECTION).find({}).toArray(function(err, docs){
-		if(err){
-			handleError(res, err.message, "Failed to get restaurants");
-		}else{
-			res.status(200).json(docs);
-		}
-	});
+
+//Test endPoint
+router.get("/myEndPoint",function(req,res){
+    res.json({"aJsonField" : "Hello World"});
 });
+
+
+
+
+var REST_COLLECTION = "restaurants";
+router.get("/restaurants", function(req,res){
+	globalDb.collection(REST_COLLECTION).find({}).toArray(function(err, docs) {
+	    if (err) {
+	      handleError(res, err.message, "Failed to retrieve restaurants");
+	    } else {
+	      res.status(200).json(docs);
+	    }
+	  });
+});
+
+
+
+//curl -H "Content-Type: application/json" -X POST -d '{"Name":"Chipotle"}' http://localhost:1234/restaurants
+
+app.post("/restaurants", function(req, res) {
+  var restaurant = req.body;
+
+  //Do some input validation 
+  if (!(req.body.Name)) {
+    handleError(res, "Invalid JSON", "restaurant name required", 400);
+  }
+
+  globalDb.collection(REST_COLLECTION).insertOne(restaurant, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Error writing restaurant to DB.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+
+/*
+So you now have a GET you can call via your Front End. 
+AngularJS - look at $http
+JQuery  - look at get() and post()
+*/
 
 app.use('/',router);
 
-app.listen(3000);
-console.log("Server is running on port 3000");
+app.listen(1234);
+console.log("Server running on 1234");
